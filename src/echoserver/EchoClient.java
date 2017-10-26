a0714f1c74fc1cb29434eb871cb0d15e8dd5a4bc
smil1201 <mill5978@morris.umn.edu>
2017-10-26 11:13:55 -0500

    Added simple server structure based on Nic McPhee's Date example.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,25 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
 
 public class EchoClient {
+
+  public static void main(String[] args) {
+    try {
+      Socket socket = new Socket("127.0.0.1", 6013);
+      InputStream input = socket.getInputStream();
+      BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+      
+      String line;
+      while ((line = reader.readLine()) != null) {
+        System.out.println(line);
+      }
+      
+      socket.close();
+    } catch (IOException ioe) {
+      System.out.println("We caught an exception");
+      System.err.println(ioe);
+    }
+  }
 }
+
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,24 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
 
 public class EchoServer {
+  public static void main(String[] args) {
+    try {
+      ServerSocket sock = new ServerSocket(6013);
+      
+      while (true) {
+        System.out.println("Got a request!");
+        Socket client = sock.accept();
+        
+        PrintWriter writer = new PrintWriter(client.getOutputStream(), true);
+        
+        writer.println("request response");
+        
+        client.close();
+      }
+    } catch (IOException ioe) {
+      System.err.println(ioe);
+    }
+  }
 }

f605f7b5df0dcdd5bd7041734efda9355fbb5695
twarling <warli010@morris.umn.edu>
2017-10-26 19:50:30 -0500

    Added functionality to echo server. All tests pass.

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -5,17 +5,37 @@ import java.io.*;
 public class EchoServer {
   public static void main(String[] args) {
     try {
+      //open socket
       ServerSocket sock = new ServerSocket(6013);
-      
+      //initialize byte var
+      byte b;
       while (true) {
+        //acknowledge and accept connection
         System.out.println("Got a request!");
         Socket client = sock.accept();
         
-        PrintWriter writer = new PrintWriter(client.getOutputStream(), true);
-        
-        writer.println("request response");
-        
+	//initialize streams 
+	OutputStream output = client.getOutputStream();
+	InputStream input = client.getInputStream();
+        //store num of bytes read
+	int i = input.read();
+
+	//loop until end of bytes read
+     	while (i != -1) {
+		//cast and write byte to client
+        	b = (byte)i;
+        	output.write(b);
+		//check for more input
+        	//System.out.println(b);
+        	i = input.read();
+	}
+      	
+	//close streams
+      	output.flush();
         client.close();
+	input.close();
+	output.close();
+
       }
     } catch (IOException ioe) {
       System.err.println(ioe);
